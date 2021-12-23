b83313fae237f89acabed747c900cbe7fc367c61
MonaHussain <Mona.Hussain.ruh.s@tuwaiq.edu.sa>
2021-12-23 08:32:18 +0300

    Mona

---
+++ NetWORKS/NetWORKS/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  NetWORKS
+//
+//  Created by Um Talal 2030 on 23/12/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iimg: UIImageView!
+    
+}
